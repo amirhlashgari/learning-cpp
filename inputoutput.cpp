6a52bf8aa02f29977d46219c1c4c699ffe900c03
amirhlashgari <amirhosseinlashgari.al@gmail.com>
2022-06-27 13:46:32 +0430

    first program with cpp

---
+++ inputoutput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+
+    std::cout << "Enter your favorite number: ";
+    std::cin >> favorite_number;
+    std:cout << "Amazing!!" << std::endl;
+
+    return 0;
+}
